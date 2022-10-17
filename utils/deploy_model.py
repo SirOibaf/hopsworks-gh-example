5420e87fdaf9488c7dea340255101621074f2d30
Fabio Buso <buso.fabio@gmail.com>
2022-10-18 01:36:31 +0200

    Add model deploy action

--- utils/deploy_model.py
+++ utils/deploy_model.py
@@ -3,6 +3,8 @@ import argparse
 
 parser = argparse.ArgumentParser(description="Utility deploy models on Hopsworks")
 parser.add_argument("--name", help="Name of the model to deploy", required=True)
+parser.add_argument("--version", help="Version of the model to deploy", required=True)
+parser.add_argument("--deployment-name", help="Name of the deployment", required=True)
 parser.add_argument(
     "--hopsworks-path",
     help="Path on Hopsworks where the predictor is stored",
@@ -17,13 +19,13 @@ project = hopsworks.login()
 mr = project.get_model_registry()
 
 # Deploy the best model according to the f1 score metric
-model = mr.get_best_model("fraud_model_us", "f1score", "max")
+model = mr.get_model(args.name, args.version)
 
 deployment = model.deploy(
-    name="fraud_model_us",
+    name=args.deployment_name,
     model_server="PYTHON",
     # serving_tool="KSERVE",
-    script_file=args.hopsworks_path,
+    script_file="hdfs:///Projects/{}/{}".format(project.name, args.hopsworks_path),
 )
 
 print("Deployment: " + deployment.name)
