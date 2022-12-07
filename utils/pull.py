da15eb226d3d9ddc2cd46ad921f36e6d7cfdf18e
Fabio Buso <buso.fabio@gmail.com>
2022-12-07 02:00:43 +0100

    Fix test pipeline to checkout the correct branch

--- utils/pull.py
+++ utils/pull.py
@@ -10,6 +10,24 @@ parser.add_argument(
     required=False,
     default="Resources",
 )
+parser.add_argument(
+    "--repository",
+    help="Repository to clone in format {github organization}/{github repository}",
+    required=False,
+    default="siroibaf/hopsworks-gh-example",
+)
+parser.add_argument(
+    "--branch",
+    help="Branch to clone, in case of PR testing",
+    required=False,
+    default="main",
+)
+parser.add_argument(
+    "--purge",
+    help="Purge existing cloned repository",
+    required=False,
+    default=False,
+)
 args = parser.parse_args()
 
 project = hopsworks.login()
@@ -17,8 +35,8 @@ git_api = project.get_git_api()
 
 # Git repository to clone
 provider = "GitHub"
-url = "https://github.com/siroibaf/hopsworks-gh-example.git"
-branch = "main"
+url = "https://github.com/{}.git".format(args.repository)
+branch = args.branch
 
 # Dataset location where to clone the repository
 folder = args.hopsworks_path
@@ -26,6 +44,12 @@ folder = args.hopsworks_path
 # Retrieve the repository metadata from Hopsworks
 try:
     repo = git_api.get_repo("hopsworks-gh-example")
+
+    if args.purge:
+        # Need to purge existing repository
+        repo.delete()
+        repo = git_api.clone(url, folder, provider, branch=branch)
+
 except:
     # Repository doesn't exists on Hopsworks. Clone it.
     repo = git_api.clone(url, folder, provider, branch=branch)
